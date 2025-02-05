001e8df7d90ebe834188e89f2e6df1ecb53ece1c
samarako99 <samarako99@gmail.com>
2025-02-05 18:36:45 +0530

    Changed base router in react-router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
     <RoomProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/neelaweralu-website/">
             <App />
         </BrowserRouter>
     </RoomProvider>,
