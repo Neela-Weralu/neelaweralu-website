db8e415ed633654b7ddaec79c98661141aa77e4e
samarako99 <samarako99@gmail.com>
2025-02-12 19:17:42 +0530

    desc changed

--- src/components/Bottomdesc.jsx
+++ src/components/Bottomdesc.jsx
@@ -7,7 +7,7 @@ function Bottomdesc() {
           <div className="bottomdesc-content">
           <p>Contact Us: +94 755500890</p>
           <p>Email: jeewanasampath11@gmail.com</p>
-          <p>&copy; 2024 Neela Weralu Pvt Ltd. All Rights Reserved.</p>
+          <p>&copy; 2025 Neela Weralu Pvt Ltd. All Rights Reserved.</p>
         </div>
 //      </bottomdesc>
     );
