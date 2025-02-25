e38bae6b12e0b02afb37f075b298bbfc12f14b90
samarako99 <samarako99@gmail.com>
2025-02-25 15:02:26 +0530

    updated

--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import room1 from "../images/room-1.jpeg"
-
+// comment updated for testing 
 const RoomDetails = () => {
     const {slug} = useParams();
     // console.log(params);  
