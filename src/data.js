9088036d1ca11a5ce317e16cde76897376d54d6d
samarako99 <samarako99@gmail.com>
2026-02-01 12:16:23 +0530

    remove1roomtype

--- src/data.js
+++ src/data.js
@@ -16,63 +16,63 @@ import img11 from "./images/room-11.jpeg";
 import img12 from "./images/room-12.jpeg";
 
 const roomsData = [
-  {
-    sys: {
-      id: "1"
-    },
-    fields: {
-      name: "single economy",
-      slug: "single-economy",
-      type: "single",
-      price: 12999,
-      size: 200,
-      capacity: 1,
-      pets: false,
-      breakfast: false,
-      featured: false,
-      description:
-        "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-      extras: [
-        "Plush pillows and breathable bed linens",
-        "Soft, oversized bath towels",
-        "Full-sized, pH-balanced toiletries",
-        "Complimentary refreshments",
-        "Adequate safety/security",
-        "Internet",
-        "Comfortable beds"
-      ],
-      images: [
-        {
-          fields: {
-            file: {
-              url: img1
-            }
-          }
-        },
-        {
-          fields: {
-            file: {
-              url: room2
-            }
-          }
-        },
-        {
-          fields: {
-            file: {
-              url: room3
-            }
-          }
-        },
-        {
-          fields: {
-            file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
-  },
+// {
+//    sys: {
+//      id: "1"
+//    },
+//    fields: {
+//      name: "single economy",
+//      slug: "single-economy",
+//      type: "single",
+//      price: 12999,
+//      size: 200,
+//      capacity: 1,
+//      pets: false,
+//      breakfast: false,
+//      featured: false,
+//      description:
+//        "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+//      extras: [
+//        "Plush pillows and breathable bed linens",
+//        "Soft, oversized bath towels",
+//        "Full-sized, pH-balanced toiletries",
+//        "Complimentary refreshments",
+//        "Adequate safety/security",
+//        "Internet",
+//        "Comfortable beds"
+//      ],
+//      images: [
+//        {
+//          fields: {
+//            file: {
+//              url: img1
+//            }
+//          }
+//        },
+//        {
+//          fields: {
+//            file: {
+//              url: room2
+//            }
+//          }
+//        },
+//        {
+//          fields: {
+//            file: {
+//              url: room3
+//            }
+//          }
+//        },
+//        {
+//          fields: {
+//            file: {
+//              url: room4
+//            }
+//          }
+//        }
+//      ]
+//    }
+//  },
   {
     sys: {
       id: "2"
@@ -757,6 +757,6 @@ const roomsData = [
 //      ]
 //    }
 //  }
-//];
+];
 
 export default roomsData;
